256e09e1d2c24fd986e102de1f504659412bfa0c
Giorgio Giannotta <jorge_upa@msn.com>
2022-10-09 13:09:50 +0200

    Fix GUI

--- test/ContentView.swift
+++ test/ContentView.swift
@@ -15,38 +15,55 @@ struct ContentView: View {
     
     var languages = ["English US", "English GB", "English ZA","Italian IT", "Spanish ES", "French FR"]
     
+    init() {
+            UITextView.appearance().backgroundColor = .clear
+        }
+
+    
     var body: some View {
-        VStack(alignment: HorizontalAlignment.center, spacing: 0) {
-            TextEditor(
-                text: $text
-                )
-            .background(Color.black)
-            .frame(height:UIScreen.main.bounds.size.height / 1.5, alignment: .center)
-            .cornerRadius(25)
-            .border(Color.blue.opacity(0.70), width: 3)
-            .lineSpacing(10)
-            .disableAutocorrection(true)
-            .padding(12)
+        ZStack {
             
-            HStack(alignment: VerticalAlignment.center, spacing: 0) {
-                Picker("Please choose a language", selection: $selectedLanguage) {
-                    ForEach(languages, id: \.self) {
-                        Text($0)
+            AsyncImage(url: URL(string: "https://pixabay.com/get/gbd3bf7f9e69ac53ce9bd948c03502b6828e068c119d8ef3507ccfd1c675102f505b989e0ba693efaaa3842b0f78bf2405c4039044e22f4529eac77be0ac52fedf6d62f72d983826f064abd7e67e8e61b_1920.jpg")) { image in
+                image.resizable()
+            } placeholder: {
+                Color.blue
+            }
+            .frame(width: .infinity, height: .infinity)
+            
+            VStack(alignment: HorizontalAlignment.center, spacing: 0) {
+                TextEditor(
+                    text: $text
+                    )
+                .background(Color.clear)
+                .frame(height:UIScreen.main.bounds.size.height / 1.5, alignment: .center)
+                .cornerRadius(25)
+                .border(Color.blue.opacity(0.70), width: 3)
+                .lineSpacing(10)
+                .disableAutocorrection(true)
+                .padding(12)
+                
+                HStack(alignment: VerticalAlignment.center, spacing: 0) {
+                    Picker("Please choose a language", selection: $selectedLanguage) {
+                        ForEach(languages, id: \.self) {
+                            Text($0)
+                        }
                     }
+                    Spacer().frame(width: 200)
+                    Button(action: {buttonPressed()}) {
+                        Text("Button")
+                    }
+                    .padding(10)
+                    .background(.blue.opacity(0.70))
+                    .foregroundColor(.white)
+                    .clipShape(Capsule())
                 }
-                Spacer().frame(width: 200)
-                Button(action: {buttonPressed()}) {
-                    Text("Button")
-                }
-                .padding(10)
-                .background(.blue.opacity(0.70))
-                .foregroundColor(.white)
-                .clipShape(Capsule())
+                
+                Spacer().frame(height: 50)
+                
             }
             
-            Spacer().frame(height: 50)
-            
         }
+        .edgesIgnoringSafeArea(.all)
     }
     
     func buttonPressed(){
